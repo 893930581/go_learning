51b1e2cfe02f90cc68d194180120da766f92f8e8
inno,com <893930581@qq.com>
2019-12-08 10:46:50 +0800

    http

---
+++ http/request/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main(){
+	resp, err := http.Get("https://www.liwenzhou.com/")
+	if err != nil {
+		fmt.Println("请求出错了,err:",err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("read response error:",err)
+		return
+	}
+	fmt.Printf("%T",body)
+}
